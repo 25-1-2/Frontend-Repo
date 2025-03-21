509fd22fa70b0ce54a35f1025185b3c78fade58c
eccho03 <choeunchae0828@gmail.com>
2025-03-21 10:53:31 +0900

    FEAT(CalanderScreen.kt) : 기본 달력 추가

--- presentation/src/main/java/com/capston/presentation/ui/CalenderScreen.kt
+++ presentation/src/main/java/com/capston/presentation/ui/CalenderScreen.kt
@@ -1,24 +1,42 @@
 package com.capston.presentation.ui
 
+import android.util.Log
+import android.widget.CalendarView
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
 fun CalenderScreen() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.Center // 정중앙에 배치
-        ) {
-            Text("calender")
-        }
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .offset(y = 100.dp),
+    ) {
+        CalendarViewExample()
     }
+}
+
+@Composable
+fun CalendarViewExample() {
+    AndroidView(
+        factory = { context ->
+            CalendarView(context).apply {
+                setOnDateChangeListener { _, year, month, dayOfMonth ->
+                    Log.d("CalendarView", "Selected date: $year-${month + 1}-$dayOfMonth")
+                }
+            }
+        },
+        modifier = Modifier.fillMaxWidth()
+    )
 }
